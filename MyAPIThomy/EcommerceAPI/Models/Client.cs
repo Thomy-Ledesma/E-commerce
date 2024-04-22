4f5d2ca51365e877a888f20d2195a7e7600fbc89
Thomás Ledesma <98626681+Thomy-Ledesma@users.noreply.github.com>
2024-04-21 23:19:40 -0300

    Changed project name
    por dios

---
+++ MyAPIThomy/EcommerceAPI/Models/Client.cs
@@ -0,0 +1,23 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace EcommerceAPI.Models
+{
+    public class Client
+    {
+        public Client(  string age, string name)
+        {
+            Name = name;
+            Age = age;
+        }
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+
+        public string Id { get; set; }
+
+        [BsonElement("name")]
+        public string Name { get; set; }
+        [BsonElement("age")]
+        public string Age { get; set; }
+    }
+}
